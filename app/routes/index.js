7776dcbe491f52557ae4c0d00a60548760b97c89
Viki <viki@mytaskteam.com>
2017-06-07 13:19:14 +0300

    Working with templates

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var landing_page = req.query.landingpage;
+  var template_path = path.join(__dirname, '..', 'templates', landing_page+'.html');
+  res.sendFile(template_path);
 });
 
 module.exports = router;
